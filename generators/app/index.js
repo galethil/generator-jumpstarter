8dd1f7f00504f4ea6e81d9de290d4f181632bd6c
Peter Baus <peter.baus@accenture.com>
2020-05-02 08:59:44 +0200

    working basic concept

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,5 @@
 var Generator = require('yeoman-generator');
-const { prompt } = require('enquirer');
+const inquirer = require('inquirer');
 
 module.exports = class extends Generator {
   // The name `constructor` is important here
@@ -9,6 +9,18 @@ module.exports = class extends Generator {
 
     // Next, add your custom code
     this.option('babel'); // This method adds support for a `--babel` flag
+
+    this.answers = {};
+
+    this.storeAnswer = (answerObject, modifierFunction) => {
+      Object.keys(answerObject).forEach((answerKey) => {
+        if (modifierFunction) {
+          answerObject[answerKey] = modifierFunction(answerObject[answerKey]);
+        }
+
+        this.answers = { ...this.answers, ...answerObject };
+      });
+    };
   }
 
   initializing() {
@@ -16,11 +28,60 @@ module.exports = class extends Generator {
   }
 
   async prompting() {
-    const response = await prompt({
+    this.storeAnswer(await inquirer.prompt({
       type: 'input',
-      name: 'username',
-      message: 'What is your username?'
-    });
+      name: 'projectName',
+      message: 'What is your project name?'
+    }));
+
+
+    this.storeAnswer(await inquirer.prompt({
+      type: 'list',
+      name: 'frontend',
+      message: 'Pick a frontend that you will use',
+      choices: ['React', 'Angular', 'Vuejs', 'other', 'no frontend']
+    }));
+
+    this.storeAnswer(await inquirer.prompt({
+      type: 'input',
+      name: 'backendServicesCount',
+      message: 'How many backend services will you use?',
+      validate: (value) => {
+        if (!value.match(/^[0-9]$/i)) {
+          return 'Please input a number between 0-9.';
+        }
+        return true;
+      }
+    }), parseInt);
+
+
+
+    // const backendServicesCount = parseInt(backendServicesCountText.backendServicesCount);
+
+    const backends = [];
+
+    for (let beCount = 0; beCount < this.answers.backendServicesCount; beCount++) {
+      const backend = await inquirer.prompt({
+        type: 'list',
+        name: 'backend',
+        message: `Pick a backend technology that you will use for service n. ${beCount + 1}`,
+        choices: ['Node.js', 'Java', 'Go', 'other']
+      });
+
+      backends.push(backend);
+    }
+
+    this.storeAnswer({ backends });
+
+    this.storeAnswer(await inquirer.prompt({
+      type: 'confirm',
+      message: 'Will you be using docker?',
+      name: 'docker',
+      default: true
+    }));
+
+    console.log(this.answers);
+
   }
 
   writing() {
