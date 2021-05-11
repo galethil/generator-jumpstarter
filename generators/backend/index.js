ce300581c89e512c5c1de7e0bec16c1e10367afd
Peter Baus <peter.baus@accenture.com>
2021-05-11 14:28:40 +0200

    Backend generator. Updating dependencies.

--- generators/backend/index.js
+++ generators/backend/index.js
@@ -11,16 +11,51 @@ module.exports = class extends Generator {
     this.subgenerator = 'backend';
     this.opts = opts;
     this.rootAnswers = opts.answers;
-    this.log()
 
   }
 
-  initializing() {
+  async initializing() {
     this.log('Backend setup');
+
+    // if there are answers passed from root we can skip this
+    if (this.rootAnswers) return;
+
+    this.rootAnswers = {};
+
+    await this.question({
+      type: 'input',
+      name: 'backendServicesCount',
+      message: 'How many backend services will you use?',
+      validate: (value) => {
+        if (!value.match(/^[0-9]$/i)) {
+          return 'Please input a number between 0-9.';
+        }
+        return true;
+      },
+      modifier: parseInt
+    });
+    this.rootAnswers.backendServicesCount = this.answers.backendServicesCount;
+
+
+    const backends = [];
+
+    for (let beCount = 0; beCount < this.answers.backendServicesCount; beCount++) {
+      const backend = await prompt({
+        type: 'list',
+        name: 'backend',
+        message: `Pick a backend technology that you will use for service n. ${beCount + 1}`,
+        choices: ['Node.js', 'Java', 'Go', 'other']
+      });
+
+      backends.push(backend);
+    }
+
+    this.storeAnswer({ backends });
+    this.rootAnswers.backends = this.answers.backends;
   }
 
   async prompting() {
-    this.clear();
+    // this.clear();
 
     const backendsDetails = [];
     for (let beCount = 0; beCount < this.rootAnswers.backends.length; beCount++) {
@@ -81,10 +116,32 @@ module.exports = class extends Generator {
   }
 
   async writing() {
+    const writeTplFile = (path, backendDetail) => this.fs.copyTpl(
+      this.templatePath(`./${backendDetail.typescript ? 'ts' : 'js'}/${backendDetail.framework}/${path}`),
+      this.destinationPath(`${this.destinationRoot()}/${backendDetail.folderName}/${path}`),
+      { ...this.answers }
+    );
+
+    const writeTplFiles = () => {};
+
     // looping throu backends
     for (let backendDetail of this.answers.backendsDetails) {
-      const mainPath = `${this.destinationRoot()}/${backendDetail.folderName}/`;
-      this.fs.write(`${mainPath}test.js`, backendDetail.folderName);
+      // const mainPath = `${this.destinationRoot()}/${backendDetail.folderName}/`;
+      // this.fs.write(`${mainPath}test.js`, backendDetail.folderName);
+      const jsExpressFiles = ['index.js'];
+      const tsExpressFiles = ['index.ts'];
+      let jsFiles;
+
+      if (backendDetail.framework === 'express') {
+        jsFiles = jsExpressFiles;
+        tsFiles = tsExpressFiles;
+      }
+      const files = backendDetail.typescript ? tsFiles : jsFiles;
+
+      for (let file of files) {
+        await writeTplFile(file, backendDetail);
+      }
+
     }
 
   }
